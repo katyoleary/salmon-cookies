79a535b3766e0026e10d1d9df6c6270988f4c922
Katy Robinson <katyorob@gmail.com>
2017-12-11 15:29:18 -0800

    got math working (kind of) for entire list, not individual times

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '
 
 var patsLocations = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
 
-//
+
 function firstAndPike() {
   var container = document.createElement('div');
   container.innerHTML = '<p>' + patsLocations[0] + '</p>';
@@ -13,7 +13,7 @@ function firstAndPike() {
   var list = document.createElement('ul');
   var listArr = [];
 
-  var cookies = getRandom(23,65) * 6.3;
+  var cookies = Math.round((Math.random() * (65 - 23) + 23) * 6.3);
 
   for (var i = 0; i < hours.length; i++){
     listArr.push('<li>' + hours[i] + cookies +  ' cookies </li>');
@@ -22,10 +22,10 @@ function firstAndPike() {
 var fullList = listArr.join('');
 list.innerHTML = fullList;
 document.body.appendChild(list);
-
 }
 
 firstAndPike();
+
 
 function seaTac() {
   var container = document.createElement('div');
@@ -35,7 +35,7 @@ function seaTac() {
   var list = document.createElement('ul');
   var listArr = [];
 
-  var cookies = getRandom(3,24) * 1.2;
+  var cookies = Math.round((Math.random() * (24 - 3) + 3) * 1.2);
 
   for(var i = 0; i < hours.length; i++) {
     listArr.push('<li>' + hours[i] + cookies + ' cookies </li>')
