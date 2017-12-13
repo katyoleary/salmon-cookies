87ddfdf96e6b0bf6c3c8dcebb1589d2b111662f5
Katy Robinson <katyorob@gmail.com>
2017-12-12 17:32:09 -0800

    GOT THE TABLE WORKING!!!!!!!!!!

--- salmonapp.js
+++ salmonapp.js
@@ -2,19 +2,47 @@
 
 var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
 
+var salmonCookiesTable = document.getElementById('salmon-cookies');
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; // via MDN docs
 }
 
 var storeLocations = [];
 
-function Cookiestand(locationName, minCustsPerHour, maxCustsPerHour, avgCookiesPerCust) {
+function makeHeaderRow() {
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  trEl.appendChild(thEl);
+
+  //hours table header
+
+  for(var i = 0; i < hours.length; i++) {
+    thEl = document.createElement('th');
+    thEl.textContent = hours[i];
+    trEl.appendChild(thEl);
+  }
+  salmonCookiesTable.appendChild(trEl);
+
+  //daily totals header
+
+  thEl = document.createElement('th');
+  thEl.textContent = 'Daily Totals';
+  trEl.appendChild(thEl);
+}
+
+makeHeaderRow();
+
+function Cookiestand(id, locationName, minCustsPerHour, maxCustsPerHour, avgCookiesPerCust) {
+  this.id = id;
   this.locationName = locationName;
   this.minCustsPerHour = minCustsPerHour;
   this.maxCustsPerHour = maxCustsPerHour;
   this.avgCookiesPerCust = avgCookiesPerCust;
+  this.totalDailySales = 0;
   this.custsEachHour = [];
   this.cookiesEachHour = [];
+  storeLocations.push(this);
   console.log(locationName);
   console.log(maxCustsPerHour);
 
@@ -26,7 +54,7 @@ function Cookiestand(locationName, minCustsPerHour, maxCustsPerHour, avgCookiesP
   },
 
   this.calcCookiesEachHour = function() {
-    // this.custsEachHour();
+    this.calcCustsEachHour();
     for(var i = 0; i < hours.length; i++) {
       var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookiesPerCust);
       console.log(oneHour, 'one hour');
@@ -35,28 +63,68 @@ function Cookiestand(locationName, minCustsPerHour, maxCustsPerHour, avgCookiesP
       this.totalDailySales += oneHour;
     }
   }
+
+
+  // REST OF THE TABLE HAPPENS HERE!!! //
+
+
+
   this.render = function(){
-      var ulEl = document.getElementById(this.locationName);
+      this.calcCookiesEachHour();
 
-      for(var i = 0; i < hours.length; i++){
+      // location name
 
-        var liEl = document.createElement('li');
-        liEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i] + ' cookies';
-        ulEl.appendChild(liEl);
+      var trEl = document.createElement('tr');
+      var tdEl = document.createElement('td');
+      tdEl.textContent = locationName;
+      trEl.appendChild(tdEl);
+
+      //num cookies each hour
+
+      for(var i = 0; i < hours.length; i++) {
+        tdEl = document.createElement('td');
+        tdEl.textContent = this.cookiesEachHour[i];
+        trEl.appendChild(tdEl);
       }
-      liEl = document.createElement('li');
-      liEl.textContent = 'Total: ' + this.totalDailySales + ' cookies';
-      ulEl.appendChild(liEl);
+       //daily totals
+
+       tdEl = document.createElement('tr');
+       tdEl.textContent = this.totalDailySales;
+       trEl.appendChild(tdEl);
+
+       salmonCookiesTable.appendChild(trEl);
     }
-    storeLocations.push(this);
 }
 
-new Cookiestand('pike', 23, 65, 6.3);
-// console.log(pike);
-new Cookiestand('seatac', 3, 24, 1.2);
-new Cookiestand('sc', 11, 38, 2.3);
-new Cookiestand('cap-hill', 20, 38, 2.3);
-new Cookiestand('alki', 2, 16, 4.6);
+
+
+
+// location instances //
+
+new Cookiestand('pike', '1st and Pike', 23, 65, 6.3);
+new Cookiestand('seatac','Seatac Airport', 3, 24, 1.2);
+new Cookiestand('sc', 'Seattle Center', 11, 38, 2.3);
+new Cookiestand('cap-hill', 'Capitol Hill', 20, 38, 2.3);
+new Cookiestand('alki', 'Alki', 2, 16, 4.6);
+
+
+
+
+for(var i = 0; i < storeLocations.length; i++) {
+  storeLocations[i].render();
+
+}
+
+ // GOING INTO TABLES NOW OKEEE //
+
+
+
+
+
+// function makePikePlaceRow() {
+//   var trEl = document.createElement('tr');
+//   thEl.
+// }
 
 
 
