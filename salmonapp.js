a31cbd81eb185e19e3e696367093ac72274a9951
Katy Robinson <katyorob@gmail.com>
2017-12-14 13:49:00 -0800

    got the form in html constructed

--- salmonapp.js
+++ salmonapp.js
@@ -35,11 +35,6 @@ function makeHeaderRow() {
 
 makeHeaderRow();
 
-//making footers total sales per hour row
-
-
-
-
 
 //constructor function
 
@@ -116,30 +111,34 @@ new Cookiestand('sc', 'Seattle Center', 11, 38, 2.3);
 new Cookiestand('cap-hill', 'Capitol Hill', 20, 38, 2.3);
 new Cookiestand('alki', 'Alki', 2, 16, 4.6);
 
-function makeFooterRow() {
-  var trEl = document.createElement('tr');
-  var tdEl = document.createElement('td');
-  trEl.appendChild(tdEl);
 
-var counter = 0;
 
-  console.log('HOURS', hours)
-  console.log('STORE LOCATIONS', storeLocations)
-  for(var i = 0; i < hours.length; i++) {
-    var hourlyTotal = 0;
-    for(var j = 0; j < storeLocations.length; j++) {
-      counter++
-      console.log('@J', storeLocations[j].cookiesEachHour)
-      // hourlyTotal += storeLocations[j].cookiesEachHour[i];
-    }
-    tdEl.innerText = hourlyTotal;
-    trEl.appendChild(tdEl);
-  }
-  salmonCookiesTable.appendChild(trEl);
-  console.log('HIIIIIIII',trEl);
-}
+//TRYING TO MAKE THE GODDAMN HOURLY TOTALS
 
-makeFooterRow();
+// function makeFooterRow() {
+//   var trEl = document.createElement('tr');
+//   var tdEl = document.createElement('td');
+//   trEl.appendChild(tdEl);
+//
+// var counter = 0;
+//
+//   console.log('HOURS', hours)
+//   console.log('STORE LOCATIONS', storeLocations)
+//   for(var i = 0; i < hours.length; i++) {
+//     var hourlyTotal = 0;
+//     for(var j = 0; j < storeLocations.length; j++) {
+//       counter++
+//       console.log('@J', storeLocations[j].cookiesEachHour)
+//       // hourlyTotal += storeLocations[j].cookiesEachHour[i];
+//     }
+//     tdEl.innerText = hourlyTotal;
+//     trEl.appendChild(tdEl);
+//   }
+//   salmonCookiesTable.appendChild(trEl);
+//   console.log('HIIIIIIII',trEl);
+// }
+//
+// makeFooterRow();
 
 for(var i = 0; i < storeLocations.length; i++) {
   storeLocations[i].render();
